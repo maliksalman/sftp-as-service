ca604440b0d3c1ead92a5192b115f1fcff4de06c
Salman Malik <smalik@pivotal.io>
2018-10-30 11:28:41 -0500

    first commit

---
+++ src/main/java/com/smalik/sftpasservice/SftpAsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.smalik.sftpasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpAsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpAsServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/smalik/sftpasservice/SftpAsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smalik.sftpasservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SftpAsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
